7a101a34393a40092d6051f83c87f5e0f336489e
bradley <bradley@schatzforensic.com.au>
2018-10-23 14:22:14 +1000

    Update copyright attribution.

--- pyaff4/container.py
+++ pyaff4/container.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from __future__ import unicode_literals
-# Copyright 2016,2017 Schatz Forensic Pty Ltd. All rights reserved.
+# Copyright 2016-2018 Schatz Forensic Pty Ltd. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License.  You may obtain a copy of
