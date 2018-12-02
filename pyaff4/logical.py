70f8b01195aec6570191d342a411c5da55c6306f
cbyneorne <cbyneorne@users.noreply.github.com>
2018-12-02 20:07:40 +0000

    Use statx to capture birth times from Linux

--- pyaff4/logical.py
+++ pyaff4/logical.py
@@ -14,8 +14,8 @@
 
 import os
 import platform
-from pyaff4 import lexicon
-from pyaff4 import rdfvalue
+from pyaff4 import lexicon, rdfvalue
+from pyaff4 import statx
 import time
 import tzlocal
 from datetime import datetime
@@ -54,25 +54,29 @@ class FSMetadata(object):
             return MacOSFSMetadata(filename, filename, size, lastWritten, accessed, recordChanged, birthTime)
         elif p == "Linux":
             size = s.st_size
-            # TODO: birthTime
             lastWritten = datetime.fromtimestamp(s.st_mtime, local_tz)
             accessed = datetime.fromtimestamp(s.st_atime, local_tz)
             recordChanged = datetime.fromtimestamp(s.st_ctime, local_tz)
-            return LinuxFSMetadata(filename, filename, size, lastWritten, accessed, recordChanged)
+
+            sx = statx.statx(filename)
+            birthTime = datetime.fromtimestamp(sx.get_btime(), local_tz)
+            return LinuxFSMetadata(filename, filename, size, lastWritten, accessed, recordChanged, birthTime)
 
 
 class LinuxFSMetadata(FSMetadata):
-    def __init__(self, urn, name, size, lastWritten, lastAccessed, recordChanged):
+    def __init__(self, urn, name, size, lastWritten, lastAccessed, recordChanged, birthTime):
         super(LinuxFSMetadata, self).__init__(urn, name, size)
         self.lastWritten = lastWritten
         self.lastAccessed = lastAccessed
         self.recordChanged = recordChanged
+        self.birthTime = birthTime
 
     def store(self, resolver):
         resolver.Set(self.urn, rdfvalue.URN(lexicon.AFF4_STREAM_SIZE), rdfvalue.XSDInteger(self.length))
         resolver.Set(self.urn, rdfvalue.URN(lexicon.standard11.lastWritten), rdfvalue.XSDDateTime(self.lastWritten))
         resolver.Set(self.urn, rdfvalue.URN(lexicon.standard11.lastAccessed), rdfvalue.XSDDateTime(self.lastAccessed))
         resolver.Set(self.urn, rdfvalue.URN(lexicon.standard11.recordChanged), rdfvalue.XSDDateTime(self.recordChanged))
+        resolver.Set(self.urn, rdfvalue.URN(lexicon.standard11.birthTime), rdfvalue.XSDDateTime(self.birthTime))
 
 
 class MacOSFSMetadata(FSMetadata):
@@ -103,3 +107,4 @@ class WindowsFSMetadata(FSMetadata):
         resolver.Set(self.urn, rdfvalue.URN(lexicon.standard11.lastWritten), rdfvalue.XSDDateTime(self.lastWritten))
         resolver.Set(self.urn, rdfvalue.URN(lexicon.standard11.lastAccessed), rdfvalue.XSDDateTime(self.lastAccessed))
         resolver.Set(self.urn, rdfvalue.URN(lexicon.standard11.birthTime), rdfvalue.XSDDateTime(self.birthTime))
+
